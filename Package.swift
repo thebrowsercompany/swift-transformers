c3929bb5c1e9545a29e9f868384fdfb3d0dd492f
Vivek Galatage <vivek@thebrowser.company>
2024-06-28 18:22:48 +0530

    Downgrading the minimium macOS version to v12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-transformers",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    platforms: [.iOS(.v16), .macOS(.v12)],
     products: [
         .library(name: "Transformers", targets: ["Tokenizers", "Generation", "Models"]),
         .executable(name: "transformers", targets: ["TransformersCLI"]),
